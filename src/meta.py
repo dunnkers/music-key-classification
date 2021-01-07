19cf3d70c2485727eed0d7c68691029f927c52c0
Thijs Havinga <thijs@thijshavinga.nl>
2021-01-07 17:42:00 +0100

    Added a hack to allow dataset downloading to continue after a 404 crash

--- src/meta.py
+++ src/meta.py
@@ -11,6 +11,9 @@ class Meta:
 
     def add_track_id(self, track_id):
         self.track_ids.append(track_id)
+    
+    def remove_track_id(self, track_id):
+        self.track_ids.remove(track_id)
 
     def dump(self, output_dir):
         with open(join(output_dir, 'meta.pickle'), 'wb') as f:
