3e652f6b2892ee1fd535f130cfa4ca1740acd130
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-02-03 13:06:54 +0100

    Change log directory 💎

--- src/peregrine.sh
+++ src/peregrine.sh
@@ -5,6 +5,8 @@
 #SBATCH --ntasks=1
 #SBATCH --partition=short
 #SBATCH --job-name=key-recognition
+#SBATCH --output=logs/slurm-%j.out
+# → do make sure /logs directory exists!
 
 module load Python/3.8.2-GCCcore-9.3.0
 pip3 install -r requirements.txt --user
