cec186b63f6c6475fa9ca8180b69e8e86a3efe9d
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-02-04 16:22:11 +0100

    Prepare new job

--- src/peregrine.sh
+++ src/peregrine.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --time=03:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem=16000
 #SBATCH --nodes=1
 #SBATCH --ntasks=10
 #SBATCH --partition=regular
 #SBATCH --job-name=key-recognition
 #SBATCH --output=logs/slurm-%A_%a.out
-#SBATCH --array=1,3,5,8,12,15
+#SBATCH --array=2,4,5,6,7,8,9,10
 # → do make sure /logs directory exists!
 
 module load Python/3.8.2-GCCcore-9.3.0
