a67c55bcf5da4cfc834144ecb0fb23dd2356a5f4
dunnkers <jeroenoverschie@gmail.com>
2021-02-04 10:59:50 +0100

    Add Peregrine hyperparameter optimization script

--- src/peregrine.sh
+++ src/peregrine.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH --time=03:00:00
-#SBATCH --mem=8000
+#SBATCH --mem=16000
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=10
 #SBATCH --partition=regular
 #SBATCH --job-name=key-recognition
 #SBATCH --output=logs/slurm-%j.out
+#SBATCH --array=1,3,5,8,12,15
 # → do make sure /logs directory exists!
 
 module load Python/3.8.2-GCCcore-9.3.0
 pip3 install -r requirements.txt --user
-python3 src/key_recognition.py --verbose --table --give-mode hmm
+python3 src/peregrine --n_components ${SLURM_ARRAY_TASK_ID}
 
