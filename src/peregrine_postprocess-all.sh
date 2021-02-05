d658e7d96798e439c24411338259ee3b751959ef
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-02-05 10:08:32 +0100

    Upload grab-all file

---
+++ src/peregrine_postprocess-all.sh
@@ -0,0 +1,5 @@
+sh src/peregrine_postprocess.sh 17824621
+sh src/peregrine_postprocess.sh 17827845
+sh src/peregrine_postprocess.sh 17827850
+sh src/peregrine_postprocess.sh 17827855
+sh src/peregrine_postprocess.sh 17827870
