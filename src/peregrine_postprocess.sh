b59199d5025440f56dfe6c4823f841a4d9dd2bb2
dunnkers <jeroenoverschie@gmail.com>
2021-02-05 10:34:34 +0100

    Push Peregrine results

--- src/peregrine_postprocess.sh
+++ src/peregrine_postprocess.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-echo "fold,n_components,error" > logs/all_slurm-$1.csv
+echo "fold,n_components,error" > results/errors-$1.csv
+echo "n_components,walltime" > results/walltime-$1.csv
 for filename in logs/slurm-$1*; do
     echo $filename
-    # cat logs/slurm-17824621_1.out | grep "Overall error: " | cut -d":" -f2 | cut -d"%" -f1 | xargs
     n_components=$(echo $filename | sed "s/logs\/slurm-$1_//" | sed "s/.out//")
     echo $n_components
 
+    # Store error % per fold
     fold=1
     errors=$(cat $filename\
      | grep "Overall error: "\
@@ -13,6 +14,10 @@ for filename in logs/slurm-$1*; do
      | sed "s/%//")
     echo $errors
     for error in $errors; do
-        echo "$fold,$n_components,$error" >> logs/all_slurm-$1.csv
+        echo "$fold,$n_components,$error" >> results/errors-$1.csv
     done
+
+    # Store walltime
+    walltime=$(grep $filename -e 'Used walltime       :' | cut -d":" -f 2- | awk '{$1=$1};1')
+    echo "$n_components,$walltime" >> results/walltime-$1.csv
 done
