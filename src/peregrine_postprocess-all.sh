b72905767eedda28a25095400d1287bc5db1d4bb
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-02-07 17:42:55 +0100

    Publish new results

--- src/peregrine_postprocess-all.sh
+++ src/peregrine_postprocess-all.sh
@@ -3,3 +3,6 @@ sh src/peregrine_postprocess.sh 17827845
 sh src/peregrine_postprocess.sh 17827850
 sh src/peregrine_postprocess.sh 17827855
 sh src/peregrine_postprocess.sh 17827870
+sh src/peregrine_postprocess.sh 17838246
+sh src/peregrine_postprocess.sh 17838254
+sh src/peregrine_postprocess.sh 17846863
